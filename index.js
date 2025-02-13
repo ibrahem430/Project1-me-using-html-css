27485453250d206113ba1304d664807c0d327b87
ibrahem <habboushebraheem@gmail.com>
2025-02-13 17:58:41 +0300

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+$(".one").on("mouseover",function(){
+   
+    $(".one").addClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
+$(".one").on("mouseout",function(){
+   
+    $(".one").removeClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
+$(".tow").on("mouseover",function(){
+   
+    $(".tow").addClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
+$(".tow").on("mouseout",function(){
+   
+    $(".tow").removeClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
+$(".three").on("mouseover",function(){
+   
+    $(".three").addClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
+$(".three").on("mouseout",function(){
+   
+    $(".three").removeClass("moveMouseOverButton");
+    // setTimeout( function(){ $("h1").removeClass("big-title")},100);
+
+})
